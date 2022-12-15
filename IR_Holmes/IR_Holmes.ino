3e5b8d97e2ff2eaaa85a6bdcd8d7e4c19770dfdc
jwke21 <jakevm21@gmail.com>
2022-12-15 12:47:38 -0800

    Updated IR_Holmes

--- IR_Holmes/IR_Holmes.ino
+++ IR_Holmes/IR_Holmes.ino
@@ -8,8 +8,8 @@
 #ifndef IR_SEND_PIN
 #define IR_SEND_PIN 3 // The pin the IR sender is connected to
 #endif
-#ifndef HTTP_PORT
-#define HTTP_PORT 80 // Arduino will listen for HTTP packets on port 80
+#ifndef PORT
+#define PORT 50500 // Arduino will listen for packets on this port
 #endif
 #define MAX_BUF_SIZE 4096
 
@@ -19,19 +19,14 @@ void iRFunctions();
 void sendHTTPSuccessResponse(EthernetClient client);
 void sendHTMLPage(EthernetClient client);
 
-uint8_t rawTicks[23] = {25,8, 25,9, 8,24, 26,7, 26,8, 8,25, 8,25, 8,25, 9,24, 9,24, 25,8, 9};  // Protocol=UNKNOWN Hash=0x3407D4AD 12 bits (incl. gap and start) received
+// Signal data transmitted to fan
+const uint16_t rawData[23] = {1230,420, 1230,470, 380,1220, 1280,370, 1280,420, 380,1270, 380,1270, 380,1270, 430,1220, 430,1220, 1230,420, 430};
+// Ethernet shield's MAC address (sticker on bottom of device)
+const byte mac[] = { 0xA8, 0x61, 0x0A, 0xAE, 0x95, 0xBE };
+// Buffer to read client's HTTP method field
+char httpMethodBuf[5] = { 0 };
 
-const uint16_t rawData[23] = {1230,420, 1230,470, 380,1220, 1280,370, 1280,420, 380,1270, 380,1270, 380,1270, 430,1220, 430,1220, 1230,420, 430};  // Protocol=UNKNOWN Hash=0x3407D4AD 12 bits (incl. gap and start) received
-
-const char prontoData[] = "0000 006D 000C 0000 0031 000F 0031 0011 0010 002D 0033 000D 0033 000F 0010 002F 0010 002F 0010 002F 0012 002D 0012 002D 0031 000F 0012 06C3";
-
-const byte mac[] = { 0xA8, 0x61, 0x0A, 0xAE, 0x95, 0xBE }; // Ethernet shield's MAC address (sticker on bottom of device)
-
-const byte ip[] = { 0x0A, 0x00, 0x00, 0x14 };
-
-char cmdBuf[5] = { 0 };
-
-EthernetServer server = EthernetServer(HTTP_PORT);
+EthernetServer server = EthernetServer(PORT); // Initializes arduino as 
 
 void setup() {
   // Set data rate to 9600 baud (i.e. bits per second) for serial data transmission
@@ -42,7 +37,7 @@ void setup() {
   // Start the IR transmitter
   IrSender.begin(IR_SEND_PIN, ENABLE_LED_FEEDBACK);
   // Start the Ethernet shield
-  Ethernet.begin(mac); // Requests given IP address from router
+  Ethernet.begin(mac); // IP address will be requested from router (via DHCP)
   // Start the server
   server.begin();
   Serial.print("Server initialized at: ");
@@ -65,14 +60,15 @@ void loop() {
         char c = client.read();
         // Read the HTTP command into buffer
         if (i < 4) {
-          cmdBuf[i++] = c;
+          httpMethodBuf[i++] = c;
         }
         if (c == '\n' && currentLineIsBlank) {
-          Serial.println(cmdBuf);
-          // If the command was a POST, then send IR signal
-          if (strcmp(cmdBuf, "POST") == 0) {
+          Serial.println(httpMethodBuf);
+          // If the HTTP method was POST, then send IR signal
+          if (strcmp(httpMethodBuf, "POST") == 0) {
             iRFunctions();
           }
+          // Send HTTP response to client
           sendHTTPSuccessResponse(client);
           sendHTMLPage(client);
           break;
@@ -85,7 +81,7 @@ void loop() {
       }
     }
     delay(1);
-    client.stop();
+    client.stop(); // Close connection
     Serial.println("Client disconnected");
   }
 }
@@ -95,11 +91,19 @@ void sendHTMLPage(EthernetClient client) {
   client.println("<html>");
   client.println("<head>");
   client.println("<title>Fan Power Control</title>\r\n");
+  client.println("<style>");
+  client.println(".container{ height: 200px; position: relative;}");
+  client.println(".center{ margin: 0; position: absolute; top: 50%; left: 50%; -ms-transform: translate(-50%, -50%); transform: translate(-50%, -50%); }");
+  client.println("</style>");
   client.println("</head>");
   client.println("<body>");
+  client.println("<div class='container'>");
+  client.println("<div class='center'>");
   client.println("    <form action='' method='post'>");
   client.println("        <input type='submit' value='Fan Power' />");
   client.println("    </form>");
+  client.println("</div>");
+  client.println("</div>");
   client.println("</body>");
   client.println("</html>\r\n");
 }
@@ -113,7 +117,7 @@ void sendHTTPSuccessResponse(EthernetClient client) {
 
 void iRFunctions() {
   // Send the code
-  Serial.println("Sending raw data");
+  Serial.println("Transmitting IR signal");
   // IrSender.sendRaw(rawTicks, sizeof(rawTicks) / sizeof(rawTicks[0]), NEC_KHZ);
   IrSender.sendRaw(rawData, sizeof(rawData) / sizeof(rawData[0]), NEC_KHZ);
   
@@ -124,13 +128,7 @@ void iRFunctions() {
 }
 
 void receiveIR() {
-  // Print the HEX code of the received IR data
-  // Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
-
-  IrReceiver.printIRResultShort(&Serial);
-  IrReceiver.printIRSendUsage(&Serial);
-
-  // Print the key value instead of HEX code
+  // Print the key value
   printKeyValue(IrReceiver.decodedIRData);
   IrReceiver.resume();
 }
